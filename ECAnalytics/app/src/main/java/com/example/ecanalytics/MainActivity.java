797caad703cb1453c631143face788b35a79a97e
javaBarista <nobles1030@gmail.com>
2019-11-26 11:17:45 +0900

    TWICE

--- ECAnalytics/app/src/main/java/com/example/ecanalytics/MainActivity.java
+++ ECAnalytics/app/src/main/java/com/example/ecanalytics/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.ecanalytics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent pushIntent = getIntent();
         setContentView(R.layout.activity_main);
+
+        Scinable sds = new Scinable(getApplicationContext(), pushIntent.getExtras());
+        sds.push("setAccountId", "hiro");
+        sds.push("setHost", "naver.com");
+        sds.push("setLanguage", "ko");
+        sds.push("addMember", "M1", "address", "남", "19941030", "99", "scinable.com", "kang", "seokmin", "1", "{1: '100', 2: '200'}");
+        sds.push("setConversion", "2", "3000","{1: '100,200', 2: '500', 5: '220'}");
+
+        sds.push("trackView");
 
     }
 
--- ECAnalytics/app/src/main/java/com/example/ecanalytics/Tracker.java
+++ ECAnalytics/app/src/main/java/com/example/ecanalytics/Tracker.java
@@ -1,88 +1,179 @@
 package com.example.ecanalytics;
 
 import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
 import android.util.Log;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Date;
+import java.util.List;
 
-import java.util.HashMap;
-import java.util.Map;
+public class Tracker extends Util {
 
-public class Tracker extends Object {
+    private Trans trans;
+    private Context context;
+    protected String jstr = null;
 
-    private Map<String, String> map = new HashMap<>();
-    private Trans trans = new Trans();
-
-    Tracker() {
+    Tracker(Context context, Bundle bundle){
+        super(context, bundle);
+        trans = new Trans(context, bundle);
+        this.context = context;
     }
 
     public String getAppId() {
-        return map.get("appId");
+        return getConfig("appId").toString();
     }
 
     private void setAppId(String appId) {
-        map.put("appId", appId);
+        setConfig("appId", appId);
     }
 
     public String getAccountId() {
-        return map.get("accountId");
+        return getConfig("accountId").toString();
     }
 
     private void setAccountId(String accountId) {
-        map.put("accountId", accountId);
+        setConfig("accountId", accountId);
     }
 
-    public String getLanguage() {
-        return map.get("language");
-    }
+    public String getLanguage() { return getConfig("language").toString(); }
 
     private void setLanguage(String language) {
-        map.put("language", language);
+        setConfig("language", language);
     }
 
+    public String getActivityTitle(){ return getConfig("activity").toString(); }
+
+    private void setActivityTitle(String activity){ setConfig("activity", activity); }
+
     public String getCampaignParameter(){
-        return map.get("campaignParameter");
+        return getConfig("campaignParameter").toString();
     }
 
     private void setCampaignParameter(String campaignParameter){
-        map.put("campaignParameter", campaignParameter);
+        setConfig("campaignParameter", campaignParameter);
     }
+
+    private void setHost(String host){
+
+        if(host.contains("http://") || host.contains("https://")){
+            host = host.substring(host.indexOf("://"));
+        }
+        setConfig("host", host);
+    }
+
+    public String getHost(){ return getConfig("host").toString(); }
 
     public void trackEvent() {
+        //현재 구동중인 서비스목록을 100개까지 획득해온다.
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> list = am.getRunningServices(100);
 
-    }
-
-    //trackpageView에 해당하는메소드
-    public void trackView() {
-
-    }
-
-    //trackView를 호출전용 push
-    @SuppressLint("LongLogTag")
-    public void push(String trigger) {
-        //사용자가 push("trackView") 명령어를 입력할 시
-        if (trigger.equals("trackView")) {
-            this.trackView();   //trackView메소드 실행
-        } else {
-            //잘못된 입력일 시 에러메시지 출력
-            Log.e("Error Message: Check your command push(", trigger + ")");
+        for(ActivityManager.RunningServiceInfo info : list) {
+            String className = info.service.getClassName();
+            String packageName = info.service.getPackageName();
         }
     }
 
-    //cancelMember를 제외한 setLanguage등 1개의 인자를 받는 push명령어 메소드
+    //trackpageView에 해당하는메소드
+    public void trackView() throws JSONException {
+
+        String vid = getVid();
+        String uid = getUid();
+
+        String ck = "";
+        String ak = "0";
+        String gk = "0";
+        String cl = "";
+        String cc = getSpref("___cc");
+        String[] carr = cc.split(",");
+
+        if(carr.length == 4){
+            ck = carr[0];
+            ak = carr[1];
+            gk = carr[2];
+            cl = carr[3];
+        }
+
+        JSONObject json = new JSONObject();
+
+        if(trans.order.size() > 0){
+            json = trans.getOrderrData();
+        }
+        else if(trans.member.size() > 0){
+            json = trans.getMemberData();
+        }
+        else if(trans.claim.size() > 0){
+            json = trans.getClaimData();
+        }
+
+        Log.d("JSON data is : ", String.valueOf(json));
+
+        Date date = new Date();
+        //int sdt = ((int) date.getTime() - (Integer) getConfig("visitTime")) / 1000;
+
+        String[] urlarr = {
+                "https://",
+                getHost(),
+                "/access",
+                "?vid=", vid,
+                "?uid=", uid,
+                "&dt=", encodeURIComponent(getActivityTitle()),
+                "&cgk=", getAccess("cgk"),
+                "&cgv=", getAccess("cgv"),
+                "&cgc=", encodeURIComponent(getAccess("cgc")),
+                "&la=", getLanguage(),
+                "&pv=", preVisitDate,
+                "&cid=", getConfig("campaign").toString(),
+                "&ck=", encodeURIComponent(ck),
+                "&ak=", ak,
+                "&gk=", gk,
+                "&cl=", encodeURIComponent(cl),
+                "&cc=", encodeURIComponent(cc),
+                "&aid=", getAccountId(),
+                "&jd=", encodeURIComponent(String.valueOf(json)),
+                "&eid=", getConfig("channel").toString(),
+                "&spv=", getConfig("pageView").toString(),
+                //"&sdt=", String.valueOf(sdt),
+                "&vc=",".", getSpref("___cvc"),"."
+        };
+
+        String url =  join(urlarr, "");
+        Log.d("url data is : ", url);
+
+        jstr = url;
+    }
+
+    //cancelMember와 setLanguage등 1개의 인자를 받는 push명령어 메소드
     @SuppressLint("LongLogTag")
     public void push(String methodName, String argument) {
 
         switch (methodName) {
+            case "setHost":
+                this.setHost(argument);
+                break;
             case "setLanguage":
                 this.setLanguage(argument);
                 break;
             case "setAccountId":
                 this.setAccountId(argument);
                 break;
+            case "setActivityTitle":
+                this.setActivityTitle(argument);
+                break;
             case "setAppId":
                 this.setAppId(argument);
                 break;
             case "setCampaignParameter":
                 this.setCampaignParameter(argument);
+                break;
+            case "cancelMember":
+                trans.type = 'W';
+                trans.cancelMember(argument);
                 break;
             default:
                 Log.e("Error Message: Check your command push(", methodName + ", value)");
@@ -104,24 +195,51 @@ public class Tracker extends Object {
                 trans.type = 'U';
                 trans.setMember(arguments);
                 break;
-            case "cancelMember":
-                trans.type = 'W';
-                trans.setMember(arguments);
-                break;
             case "addItem":
                 trans.addItem(arguments);
                 break;
             case "addClaim":
                 trans.type = 'C';
-                //trans.addClaim(arguments);
+                trans.addClaim(arguments);
                 break;
             case "addTrans":
                 trans.type = 'C';
-
+                trans.addTrans(arguments);
+                break;
+            case "setCustomVar":
+                trans.setCustomVar(arguments);
+                break;
+            case "setConversion":
+                trans.setConversion(arguments);
                 break;
             default:
                 Log.e("Error Message: Check your command push(", methodName + ", values)");
                 break;
         }
+    }
+
+    //자바의 URLENCODER를 자바스크립트와 동일한 결과가 나오게 변경
+    private static String encodeURIComponent(String s)
+    {
+        String result = null;
+
+        try
+        {
+            result = URLEncoder.encode(s, "UTF-8")
+                    .replaceAll("\\+", "%20")
+                    .replaceAll("\\%21", "!")
+                    .replaceAll("\\%27", "'")
+                    .replaceAll("\\%28", "(")
+                    .replaceAll("\\%29", ")")
+                    .replaceAll("\\%7E", "~");
+        }
+
+        // This exception should never occur.
+        catch (UnsupportedEncodingException e)
+        {
+            result = s;
+        }
+
+        return result;
     }
 }
