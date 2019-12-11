f55057e3744777d999f68f6f5f0f80e86021778a
YangTaeSung <tsy0668@gmail.com>
2019-12-11 11:29:09 +0900

    Add SCTracker2-AARTest and Fix SCTracker2 for module

---
+++ SCTracker2AARTest/app/src/main/java/com/example/sctracker2_aartest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sctracker2_aartest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sctracker2.*; // 라이브러리 사용 가능
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
